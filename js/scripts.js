e87a16252c21b9b521167309e617d6fdd1cb7c4d
Sogol <108494457+Sogolsa@users.noreply.github.com>
2023-11-25 20:53:46 -0800

    Fixed formatting
    1. Defined the variable with let.
    2. Added single quotes to the strings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-pokemonList = [
-    {name: Bulbasaur, height: 0.7, types:[grass, poison]},
-    {name: Charizard, height: 1.7, types: [fire, flying]},
-    {name: Starmie, height: 1.1, types: [psychic, water]}
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
+  { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
+  { name: 'Starmie', height: 1.1, types: ['psychic', 'water'] }
 ];
+
+
+
