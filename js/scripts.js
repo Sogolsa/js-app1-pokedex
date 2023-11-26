28976ffcb5053ffee57f913733299a160c1a06db
Sogol <108494457+Sogolsa@users.noreply.github.com>
2023-11-26 08:17:06 -0800

    Created a for loop for pokemonList array...
    To write each pokemon's name with their height next to the names.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,8 @@ let pokemonList = [
 
 
 
+for (let i =0; i < pokemonList.length; i++){
+    document.write(
+        pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + ' '
+    );
+}
