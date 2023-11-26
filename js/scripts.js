12c8732f286577b6118646263fa041aced8ed42a
Sogol <108494457+Sogolsa@users.noreply.github.com>
2023-11-26 08:53:22 -0800

    Highlighted the Pokemon with height > 1.5
    part 3: Created a for loop with if condition of height > 1.5 to label the Pokemon that meets the condition.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,16 @@ let pokemonList = [
   { name: 'Starmie', height: 1.1, types: ['psychic', 'water'] }
 ];
 
+/* for loop to list the names of all pokemons in the array with their height,
+ and if height > 1.5, it get's the label of 'Wow, that's big!' */
 
-
-for (let i =0; i < pokemonList.length; i++){
-    document.write(
-        pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + ' '
-    );
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height > 1.5) {
+        nameHeight = pokemonList[i].name + ' ' +
+         '(height:' + pokemonList[i].height + ')';
+        document.write(nameHeight + ' ' + "- Wow, that's big!" + '<br>');
+    } else {
+        document.write(pokemonList[i].name + ' ' + 
+        '(height:' + pokemonList[i].height + ')' + '<br>');
+    }
 }
