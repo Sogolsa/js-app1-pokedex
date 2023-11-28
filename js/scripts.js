b71b189784ba789093665192bf9881ae3be27f8f
Sogol <108494457+Sogolsa@users.noreply.github.com>
2023-11-27 18:36:34 -0800

    Used a forEach() method instead of for loop
    Task5, part1

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
 let pokemonList = [
   { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
   { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
-  { name: 'Starmie', height: 1.1, types: ['psychic', 'water'] }
+  { name: 'Starmie', height: 1.1, types: ['psychic', 'water'] },
 ];
 
-/* for loop to list the names of all pokemons in the array with their height,
- and if height > 1.5, it get's the label of 'Wow, that's big!' */
+/* Using forEach() method to replace the for loop to get the same output */
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.5) {
-    nameHeight =
-      pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')';
-    document.write(nameHeight + ' ' + "- Wow, that's big!" + '<br>');
+// pokemonList.forEach(function (pokemon) {
+//   document.write(pokemon.name + ' ' + '(height:' + pokemon.height + ') <br>');
+// });
+
+// Highlighting the biggest pokemon //
+pokemonList.forEach(function (pokemon) {
+  if (pokemon.height > 1.5) {
+    nameHeight = pokemon.name + ' ' + '(height:' + pokemon.height + ')';
+    document.write(nameHeight + " -Wow! That's big!" + '<br>');
   } else {
     document.write(
-      pokemonList[i].name +
-        ' ' +
-        '(height:' +
-        pokemonList[i].height +
-        ')' +
-        '<br>'
+      pokemon.name + ' ' + '(height:' + pokemon.height + ')' + '<br>'
     );
   }
-}
+});
