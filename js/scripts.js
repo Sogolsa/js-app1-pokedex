154be5b59be281e5e9a3f2aa39f26f5602a7d4c8
Sogol <108494457+Sogolsa@users.noreply.github.com>
2023-11-29 16:19:14 -0800

    Added buttons of list of pokemon names that when you click on the button you see the pokemon name on the console.

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,70 @@
-let pokemonList = [
-  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
-  { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
-  { name: 'Starmie', height: 1.1, types: ['psychic', 'water'] },
-];
-
-/* Using forEach() method to replace the for loop to get the same output */
-
-// pokemonList.forEach(function (pokemon) {
-//   document.write(pokemon.name + ' ' + '(height:' + pokemon.height + ') <br>');
-// });
-
-// // Highlighting the biggest pokemon //
-// pokemonList.forEach(function (pokemon) {
-//   if (pokemon.height > 1.5) {
-//     nameHeight = pokemon.name + ' ' + '(height:' + pokemon.height + ')';
-//     document.write(nameHeight + " -Wow! That's big!" + '<br>');
-//   } else {
-//     document.write(
-//       pokemon.name + ' ' + '(height:' + pokemon.height + ')' + '<br>'
-//     );
-//   }
-// });
-
 // IIFE //
-let pokemonRepository = (function (pokemon) {
+// Pokemon Array of Objects //
+let pokemonRepository = (function () {
   let pokemonList = [
     { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] },
     { name: 'Charizard', height: 1.7, types: ['fire', 'flying'] },
     { name: 'Starmie', height: 1.1, types: ['psychic', 'water'] },
   ];
 
+  // Add a new object ot the array and make sure it's object //
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (typeof pokemon === 'object') {
+      pokemonList.push(pokemon);
+    } else {
+      console.error('pokemon mubst be an object.');
+    }
   }
 
+  // Get the pokemon List //
   function getAll() {
     return pokemonList;
+  }
+
+  // show details when click on the button //
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  // button listener so when click on the button, it shows the details of pokemon //
+  function addButtonListener(button, pokemon) {
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    })
+  }
+
+  // Add lists of pokemon buttons //
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let pokemonItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    
+    addButtonListener(button, pokemon);
+
+    pokemonItem.appendChild(button);
+    pokemonList.appendChild(pokemonItem);
   }
 
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
+    addButtonListener: addButtonListener
   };
 })();
 
+
+// Add a new object to the pokemon list //
 pokemonRepository.add({
   name: 'Jigglypuff',
   height: 0.5,
   types: ['normal', 'fairy'],
 });
-let newPokemonList = pokemonRepository.getAll();
 
-newPokemonList.forEach(function (pokemon) {
-  if (pokemon.height > 1.5) {
-    nameHeight = pokemon.name + ' ' + '(height:' + pokemon.height + ')';
-    document.write(nameHeight + " -Wow! That's big!" + '<br>');
-  } else if (pokemon.height <= 0.5) {
-    nameHeight = pokemon.name + ' ' + '(height:' + pokemon.height + ')';
-    document.write(nameHeight + " -Wow! That's small!" + '<br>');
-  } else {
-    document.write(
-      pokemon.name + ' ' + '(height:' + pokemon.height + ')' + '<br>'
-    );
-  }
+// Go trought all the list and get the names one by one. //
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
